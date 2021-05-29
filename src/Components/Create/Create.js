f96f95d0223a710916eb7c73eb0055e6c98dac39
Berat Kales <bkales123@gmail.com>
2021-05-28 20:17:06 -0400

    Added Create-Components folder for organization

--- src/Components/Create/Create.js
+++ src/Components/Create/Create.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import './Create.css'
-import PollTitle from './Poll-Title/PollTitle'
-import PollOption from './Poll-Option/PollOption'
+import PollTitle from './Create-Components/PollTitle'
+import PollOption from './Create-Components/PollOption'
+import PollSubmit from './PollSubmit'
 function Create() {
     return (
         <div className="Create">
         <form className="Create__Form">
             <PollTitle/>
             <PollOption/>
+            <PollOption/>
+            <PollOption/>
+            <PollOption/>
+            <PollOption/>
+            <PollSubmit/>
+
         </form>
     </div>
     )
---
+++ src/Components/Create/PollSubmit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PollSubmit() {
+    return (
+        <div>
+            <input className="Create__Submit" type="submit" placeholder="Create Poll"></input>
+        </div>
+    )
+}
+
+export default PollSubmit
