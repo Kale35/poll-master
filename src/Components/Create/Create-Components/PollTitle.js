43a36d60a24843f0ce8215de3497d1985ba8df94
deaviss <deaviss@gmail.com>
2021-05-29 19:37:17 +0200

    first api call

--- src/Components/Create/Create-Components/PollTitle.js
+++ src/Components/Create/Create-Components/PollTitle.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
-function PollTitle() {
+function PollTitle(props) {
+    const update = e => {
+        const setQuestion = props.question;
+        setQuestion(e.target.value)
+    }
     return (
         <div>
-            <input className="Create__Title" type="text" placeholder="Poll Title"></input>
+            <input
+                className="Create__Title"
+                type="text"
+                placeholder="Poll Title"
+                minLength={3}
+                required={true}
+                onChange={(e) => update(e)}
+            ></input>
         </div>
     )
 }
--- src/Components/Create/Create.js
+++ src/Components/Create/Create.js
@@ -6,29 +6,60 @@ import PollSubmit from "./PollSubmit";
 import AddMoreOptionsButton from "./AddMoreOptionsButton";
 
 function Create() {
-    const [pollAmount, setPollAmount] = useState(0);
+  const [pollAmount, setPollAmount] = useState(3);
+  const [question, setQuestion] = useState("");
+  const [answers, setAnswers] = useState([
+    "", "", ""
+  ]);
 
-    return (
-        <div className="Create">
-            <form className="Create__Form">
-                <PollTitle />
-                <PollOption />
-                <PollOption />
-                <PollOption />
+  function update(index, event) {
+    const newAnswers = [...answers];
+    newAnswers[index] = event;
+    setAnswers(newAnswers);
+  }
 
-                {Array.from(Array(pollAmount), (e, i) => {
-                    return <PollOption />;
-                })}
-                <div className="Create__Buttons">
-                    <PollSubmit />
-                    <AddMoreOptionsButton
-                        pollAmount={pollAmount}
-                        setPollAmount={setPollAmount}
-                    />
-                </div>
-            </form>
+  const handleSubmit = async e => {
+    // e.preventDefault();
+    const tmpAnswers = [];
+    if (question.length > 2 && answers.length > 0) {
+      answers.forEach(e => {
+        tmpAnswers.push({ answer: e });
+      });
+      const response = await fetch("http://localhost:1234/createPoll", {
+        method: "POST",
+        headers: {
+          "Accept": 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          question,
+          answers: tmpAnswers
+        })
+      })
+      const content = await response.json();
+
+      // console.log(content);
+    }
+  }
+
+  return (
+    <div className="Create">
+      <form className="Create__Form" onSubmit={handleSubmit}>
+        <PollTitle question={(ev) => setQuestion(ev)} />
+
+        {Array.from(Array(pollAmount), (e, i) => (
+          <PollOption key={i} update={(ev) => update(i, ev)} />
+        ))}
+        <div className="Create__Buttons">
+          <PollSubmit />
+          <AddMoreOptionsButton
+            pollAmount={pollAmount}
+            setPollAmount={setPollAmount}
+          />
         </div>
-    );
+      </form>
+    </div>
+  );
 }
 
 export default Create;
