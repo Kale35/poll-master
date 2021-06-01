d0c553f673b13a5804e7ce0d4df417d551b631ba
user name test <andynoobprogrammer@gmail.com>
2021-06-01 01:38:12 -0700

    added charts

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,45 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import PollCard from '../Components/HomeComponents/PollCard/PollCard'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import PollCard from "../Components/HomeComponents/PollCard/PollCard";
 
 export default function Home() {
+	const [data, setData] = useState([]);
 
-	const [data, setData] = useState([])
-
-	// useEffect(async () => {
-	// 	const resp = await fetch("http://localhost:1234/poll/random/7")
-	// 	setData(await resp.json())
-	// }, [])
-
-
-	//
 	useEffect(async () => {
-		const response = await fetch("http://localhost:1234/getAllPolls");
-		const data = await response.json();
-		setData(data);
-	}, [])
-
+		// await fetch("http://localhost:1234/getAllPolls");
+		const resp = await fetch("http://localhost:1234/poll/random/7");
+		setData(await resp.json());
+	}, []);
 
 	const showData = () => {
-		console.log(data)
-		if (data.length === 0)
-			return <div>Loading...</div>
+		console.log(data);
+		if (data.length === 0) return <div>Loading...</div>;
 		else if (data.length > 0)
 			return (
-				<div style={{ display: 'flex', flexWrap: "wrap", justifyContent: "center" }}>
+				<div
+					style={{
+						display: "flex",
+						flexWrap: "wrap",
+						justifyContent: "center",
+					}}
+				>
 					{data.map((e, i) => (
 						<PollCard key={i} data={e} />
 					))}
 				</div>
-			)
-	}
+			);
+	};
 
 	return (
 		<div>
 			<Link to="/create">Create</Link>
-			<div>
-				{showData()}
-			</div>
+			<div>{showData()}</div>
 		</div>
-	)
+	);
 }
